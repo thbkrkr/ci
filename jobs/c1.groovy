9be45a48209f332d9d941548e8862679f8c4944b
thbkrkr <thb.krkr@gmail.com>
2017-06-07 00:25:20 +0200

    c1 job

---
+++ jobs/c1.groovy
@@ -0,0 +1,17 @@
+job('build_c1_master') {
+    scm {
+        git {
+            remote {
+                url('git@bitbucket.org:thb/c1.git')
+                credentials('ci-bitbucket.id_rsa')
+            }
+            branch('master')
+        }
+    }
+    triggers {
+        scm('H * * * *')
+    }
+    steps {
+        shell('doo b')
+    }
+}
