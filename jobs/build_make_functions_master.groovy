3f713d7710a72471fe4c87fe8e3e0b292e04f2b8
thbkrkr <thb.krkr@gmail.com>
2017-06-14 12:53:23 +0200

    Add build_make_functions_master job

---
+++ jobs/build_make_functions_master.groovy
@@ -0,0 +1,32 @@
+freeStyleJob('functions') {
+    displayName('functions')
+    description('')
+
+    properties {
+        githubProjectUrl('https://github.com/thbkrkr/functions')
+    }
+
+    logRotator {
+        numToKeep(20)
+        daysToKeep(30)
+    }
+
+    scm {
+        git {
+            remote {
+                url('https://github.com/thbkrkr/functions')
+                credentials('public')
+            }
+            branches('*/master')
+        }
+    }
+
+    triggers {
+        cron('H H * * *')
+        githubPush()
+    }
+
+    steps {
+        shell('make deploy')
+    }
+}
